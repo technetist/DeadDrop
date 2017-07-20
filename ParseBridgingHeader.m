2fa0af6e05eb731e736992394228a47a9ca5a230
Adrien Maranville <Adrien.Maranville@gmail.com>
2017-07-20 11:23:16 -0600

    integrated Parse with CocoaPods

---
+++ ParseBridgingHeader.m
@@ -0,0 +1,9 @@
+//
+//  ParseBridgingHeader.m
+//  Dead Drop
+//
+//  Created by Adrien Maranville on 7/20/17.
+//  Copyright © 2017 Adrien Maranville. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
